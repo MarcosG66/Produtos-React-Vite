6ba8fce6bda351e3cfa75f7eaf5d5993aad208c3
Marcos Gentili <marcosgentili66@gmail.com>
2023-08-30 20:35:30 -0300

    Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,136 +1,27 @@
+import { useEffect, useState } from 'react';
+import ListaProdutos from "./ListaProdutos";
 
-const PRODUCTS = [
-  {
-    id: 1,
-    name: 'Camiseta Básica',
-    price: '$35',
-    color: 'Preta',
-    image: {
-      src: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      alt: 'Camiseta preta básica com gola redonda e manga curta.',
-    },
-  },
-  {
-    id: 2,
-    name: 'Camiseta Básica',
-    price: '$40',
-    color: 'Branco Aspen',
-    image: {
-      src: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg',
-      alt: 'Camiseta branca básica com gola redonda e manga curta.',
-    },
-  },
-  {
-    id: 3,
-    name: 'Camiseta Básica',
-    price: '$39',
-    color: 'Carvão',
-    image: {
-      src: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg',
-      alt: 'Camiseta cinza básica com gola redonda e manga curta.',
-    },
-  },
-  {
-    id: 4,
-    name: 'Camiseta Básica',
-    price: '$35',
-    color: 'Pontos',
-    image: {
-      src: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg',
-      alt: 'Camiseta branca básica com gola redonda e manga curta.',
-    },
-  },
-]
+const API_URL = 'https://products-api-i6m5.onrender.com/products'
 
 function App() {
+  const [listaProdutos, setListaProdutos] = useState([]);
+  const [itensCarrinho, setItensCarrinho] = useState(0);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setListaProdutos(data);
+      }); 
+  },[]);              
+  
   return (
     <div className="min-h-screen bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-yellow-600 via-purple-200 to-teal-200">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Items mais vendidos</h2>
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-
-          <div className="group relative">
-            <div
-              className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href="#">
-                    <span aria-hidden="true" className="absolute inset-0"></span>
-                    Camiseta Básica
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">Preta</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">R$ 35</p>
-            </div>
-          </div>
-
-          <div className="group relative">
-            <div
-              className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg"
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href="#">
-                    <span aria-hidden="true" className="absolute inset-0"></span>
-                    Camiseta Básica
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">Branco Aspen</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">R$ 40</p>
-            </div>
-          </div>
-
-          <div className="group relative">
-            <div
-              className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg"
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href="#">
-                    <span aria-hidden="true" className="absolute inset-0"></span>
-                    Camiseta Básica
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">Carvão</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">R$ 39</p>
-            </div>
-          </div>
-
-          <div className="group relative">
-            <div
-              className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg"
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-gray-700">
-                  <a href="#">
-                    <span aria-hidden="true" className="absolute inset-0"></span>
-                    Camiseta Básica
-                  </a>
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">Pontos</p>
-              </div>
-              <p className="text-sm font-medium text-gray-900">R$ 35</p>
-            </div>
-          </div>
-
-        </div>
-      </div>
+      <h2 className='text-xl font-semibold tracking-tight text-gray tac'>Itens no carrinho {itensCarrinho}</h2>
+      {listaProdutos &&
+      <ListaProdutos 
+      listaProdutos={listaProdutos} 
+      onClick={() => setItensCarrinho(itensCarrinho + 1)}/>}
     </div>
   );
 }
